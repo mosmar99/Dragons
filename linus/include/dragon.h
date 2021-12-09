4a1d07cb05837e3e24d851b90de4ce31504d5f2b
Linkan42 <sali20jt@student.ju.se>
2021-12-09 17:36:20 +0100

    revised structure in loadDatabase()

--- linus/include/dragon.h
+++ linus/include/dragon.h
@@ -15,7 +15,7 @@ typedef struct Dragon
     unsigned int id;            // id
     char *name;                 // name
     char isVolant;              // 'Y' --> it is able to fly, 'N' --> it is not able to fly
-    unsigned int fierceness;    // fierceness, higher => more fierce
+    unsigned int fierceness;    // fierceness, higher => more fierce (scale of 1 to 10)
     unsigned int numColours;    // number of colours
     char *colours[MAX_COLOURS]; // which colours
 } Dragon;
