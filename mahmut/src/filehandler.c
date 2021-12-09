84a47dd21a88189a460a7c28e1cbdc060355e707
MahmutOsmanovic <47375043+MahmutOsmanovic@users.noreply.github.com>
2021-12-09 22:25:43 +0100

    up

--- mahmut/src/filehandler.c
+++ mahmut/src/filehandler.c
@@ -13,28 +13,28 @@ void loadDatabase(char *filename, Database *db)
     int id, colourCount, fierceness, nextId; char volant; // used to check validity of input
     for (int drgIdx = 0; drgIdx < (*db).size && !feof(drgFilePtr); drgIdx++) {
 
-        // read id
+        // get id
         fscanf(drgFilePtr, "%d", &id), assert(id > 0); // read, save and check input
         (*db).dragons[drgIdx].id = id; // init idx to positively read input
 
-        // read name
+        // get name
         (*db).dragons[drgIdx].name = calloc(MAX_NAME, sizeof(char));
         if(!(*db).dragons[drgIdx].name) puts("Err: Failed to allocate memory for dragon name"), exit(-1);
         fscanf(drgFilePtr, "%49s", (*db).dragons[drgIdx].name);
 
-        // read volant
+        // get volant
         fscanf(drgFilePtr, "%s", &volant), assert(volant == 'Y' || volant == 'N'); // read, check
         (*db).dragons[drgIdx].isVolant = volant;
 
-        // read fierceness
+        // get fierceness
         fscanf(drgFilePtr, "%d", &fierceness), assert(fierceness >= 1 && fierceness <= 10);
         (*db).dragons[drgIdx].fierceness = fierceness;
 
-        // read # of colours
+        // get # of colours
         fscanf(drgFilePtr, "%d", &colourCount), assert(colourCount > 0 && colourCount <= MAX_COLOURS);
         (*db).dragons[drgIdx].numColours = colourCount;
 
-        // all the colours
+        // get all the colours
         for (int currCol = 0; currCol < colourCount && currCol < MAX_COLOURS && !feof(drgFilePtr); currCol++) {
             (*db).dragons[drgIdx].colours[currCol] = calloc(MAX_COLOUR_NAME, sizeof(char));
             if (!(*db).dragons[drgIdx].colours[currCol]) puts("Err: Failed to allocate memory forc dragon color"), exit(-1);
